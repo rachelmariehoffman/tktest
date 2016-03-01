f136416ce9a6027f3ef81cd6410c179ce8970ec7
rhoffmanssf <rhoffman@softstackfactory.org>
2016-03-01 01:33:00 +0000

    sort function try #4

--- www/js/sortServices.js
+++ www/js/sortServices.js
@@ -3,16 +3,36 @@ angular.module('SortModule', [])
     var service = this;
 
     service.sortArray = function(testArray) {
-        for (var i = 0; i < testArray.length; i++) {
+
+        var repeat = false;
+        do {
+            repeat = false;
             for (var i = 0; i < testArray.length; i++) {
                 if (testArray[i] > testArray[i++]) {
                     var temp = testArray[i];
                     var temp2 = testArray[i++];
                     testArray[i] = temp2;
                     testArray[i++] = temp;
+                    repeat = true;
                 }
             }
+        } while (repeat);
             return testArray;
-        }
     };
 }]);
+
+
+
+/*
+for (var i = 0; i < testArray.length; i++) {
+    for (var i = 0; i < testArray.length; i++) {
+        if (testArray[i] > testArray[i++]) {
+            var temp = testArray[i];
+            var temp2 = testArray[i++];
+            testArray[i] = temp2;
+            testArray[i++] = temp;
+        }
+    }
+    return testArray;
+}
+*/
